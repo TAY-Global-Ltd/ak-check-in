62e3ed7ccf365cc9a7937c0a43b392416fc318b7
Tony Yum <tony.yum@gmail.com>
2024-01-31 22:46:50 +0900

    feat(subscription): Added subscription info in request server and UI now takes that information to subscription PubNub

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import "./App.css";
 import UsersTable from "./components/UsersTable";
 import Timer from "./components/Timer";
+import { useEffect } from "react";
+import PubNub from 'pubnub';
 
 const currentClass = "BJJ";
 
 const App = () => {
+
+  useEffect(() => {
+
+    const subscribeToUpdates = async (channel, subscribeKey, uuid) => {
+      const pubnub = new PubNub({
+        subscribeKey: subscribeKey,
+        uuid: uuid
+      });
+
+      pubnub.addListener({
+        message: function (event) {
+          console.log("~~~ event", event.message);
+        },
+        presence: function (presenceEvent) {
+          console.log('~~~ Presence Event:', presenceEvent);
+        },
+      });
+
+      pubnub.subscribe({
+        channels: [channel],
+        withPresence: true,
+      });
+    }
+
+    const initApp = async () => {
+      const res = await fetch('http://127.0.0.1:8765/initial_state');
+      const initialState = await res.json();
+      const subInfo = initialState.subscription_info
+      subscribeToUpdates(subInfo.channel, subInfo.subscribe_key, subInfo.user_id)
+      document.title = "AK Attendance";
+    }
+
+    initApp();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -3,35 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PubNub from 'pubnub';
-// import { PubNubProvider, usePubNub } from 'pubnub-react';
-
-const pubnub = new PubNub({
-  publishKey: process.env.PUB_NUB_PUBLISH_KEY,
-  subscribeKey: process.env.PUB_NUB_SUBSCRIBE_KEY,
-  uuid: "myFirstUser",
-});
-
-pubnub.subscribe({
-  channels: ["ak-check-in"],
-  withPresence: true,
-});
-
-pubnub.addListener({
-  message: function (event) {
-    console.log("~~~ event", event.message);
-  },
-  presence: function (presenceEvent) {
-    console.log('~~~ Presence Event:', presenceEvent);
-  },
-});
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// avoid using React.StrictMode because it will render twice and hits the server twice
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
